a4fbfc2f967fcf64203542f7280e57b3e40d6cda
Aabhash Gauda <aabhash@aeologic.com>
2023-12-14 19:06:56 +0530

    ci cd inital

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.port || 9000;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/cicd", (req, res) => {
+  res.json({
+    message: "welcome to cicd deployment"
+  })
+})
+
+
+app.listen(port, (error) => {
+  error ? console.log(`Server not started`) : console.log(`Server started at port ${port}`);
+});
